365b5424d992b779dc77f1b2b72f96fd80d2e374
bocchiko <luisbonilla0908@gmail.com>
2023-10-11 17:47:22 -0600

    Navigator

--- navegacion/src/screens/Home.js
+++ navegacion/src/screens/Home.js
@@ -1,12 +1,17 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, Text, Button} from 'react-native';
 
-export default function Home(){
+export default function Home(props){
+    const {navigation} = props;
+    
     return(
         <View>
             <Text>Estamos en Home</Text>
+            <Button
+                title='Ir a About'
+                onPress={() => navigation.navigate('about')}
+            />
         </View>
-
     );
 
 }
